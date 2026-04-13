d661f73270aee0100544439b945cf500e44d30ce
Elias <156465564+eligonzata@users.noreply.github.com>
2026-04-13 16:07:51 -0500

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,19 @@
+-- delete trigger that checks for a patients active prescriptions before deletion
+DELIMITER //
+CREATE TRIGGER tr_patient_pres
+BEFORE DELETE
+ON patient
+FOR EACH ROW
+BEGIN
+	-- check if patient has any active prescriptions before deleting
+    IF EXISTS(
+		SELECT 1
+         FROM prescription 
+         WHERE patient_id = OLD.patient_id AND status = 'Active'
+    ) THEN
+		-- block delete
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Failed to delete patient. Patient has at least one active prescription.';
+    END IF;
+END //
+DELIMITER ;
