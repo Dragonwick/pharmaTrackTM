2fdb21c1b0f8ac2fc9e050c545b2af51cf5bd49d
aaron <aaron@MacBookPro.lan>
2026-04-13 17:00:39 -0500

    removed additional file and added the procedure

--- triggers_procedure_function.sql
+++ triggers_procedure_function.sql
@@ -71,3 +71,38 @@ DELIMITER ;
 -- DELETE TRIGGER EXAMPLE STATEMENT
 DELETE FROM patient
 WHERE patient_id = 51;
+
+-- stored procedure: get_patient_summary
+-- given a patient ID, returns all prescriptions for
+-- 	that patient along with the medications prescribed
+--  and the current status of each prescription.
+-- inpu:  p_patient_id (INT) - the ID of the patient to look up
+-- usage:  CALL get_patient_summary(1);
+
+DROP PROCEDURE IF EXISTS get_patient_summary;
+
+DELIMITER //
+
+CREATE PROCEDURE get_patient_summary(IN p_patient_id INT)
+BEGIN
+    SELECT
+        CONCAT(p.first_name, ' ', p.last_name) AS "Patient Name",
+        pr.prescription_id,
+        pr.date_issued,
+        pr.status,
+        pr.refills_remaining,
+        i.medication_name,
+        pl.dosage_instructions
+    FROM patient p
+    JOIN prescription pr ON p.patient_id = pr.patient_id
+    JOIN prescription_line pl ON pr.prescription_id = pl.prescription_id
+    JOIN medication m ON pl.medication_id = m.medication_id
+    JOIN inventory i ON m.inventory_id = i.inventory_id
+    WHERE p.patient_id = p_patient_id
+    ORDER BY pr.date_issued DESC;
+END //
+
+DELIMITER ;
+
+-- this is an example to retrieve full prescription summary for patient ID 1 (Kevin Browning)
+CALL get_patient_summary(1);
